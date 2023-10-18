6a96cfda83f11511e0a5c6f08c36ff480ded6f88
vlad <wewef@mail.ru>
2023-10-18 15:47:55 +0200

    181023

--- HomeWork/js1/script.js
+++ HomeWork/js1/script.js
@@ -1,10 +1,34 @@
 // alert("Hello")
 // console.log(100) 
 
-let name = "Vlad"
-name = "Arsen"
-console.log(name)
+// let name = "Vlad"
+// name = "Arsen"
+// console.log(name)
 
-const userName = "Vlad"
+// const userName = "Vlad"
 // userName ="Artem"
-console.log(userName)
+// console.log(userName)
+
+// const number=prompt ("введите число")
+// console.log (typeof number)
+// const username ="Arsen"
+// const surname = "Iusupov"
+// console.log (username+ "Iusupov")
+// console.log (`Имя: ${username}, Фамилия: ${surname}`)
+// const name= prompt("Введите имя")
+// console.log (`Привет ${name}`)
+// const r = 100
+// const g = 57
+// const b = 141
+// console.log(`rgb ( ${r} , ${g} , ${b})`)
+// const data = prompt("Введите ваш год рождения: ")
+// const age = 2023-data
+// console.log (`Ваш возраст ${age}`)
+
+const degree = 20
+if (degree > 20) {
+    console.log("Надеть футболку")
+} else {
+    console.log("Надеть шубу")
+
+}
