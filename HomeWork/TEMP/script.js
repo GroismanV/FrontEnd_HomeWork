c2e8ae28846325f1bff5c6f121aa48811ddf2970
vlad <wewef@mail.ru>
2023-12-10 21:43:53 +0100

    ---

---
+++ HomeWork/TEMP/script.js
@@ -0,0 +1,16 @@
+function method1() {
+    console.log("Button 1 clicked!");
+  }
+  
+  function method2() {
+    console.log("Button 2 clicked!");
+  }
+  
+  function method3() {
+    console.log("Button 3 clicked!");
+  }
+  
+  function method4() {
+    console.log("Button 4 clicked!");
+  }
+  
